89377e3d23e859c92f0797475ee0ea4e7d173bdf
DanielAZ <danielabezarate@gmail.com>
2021-12-02 23:17:46 +0100

    Creados la barra de menú básica, junto a los botones de ordenar y el Acerca de

--- app/src/main/java/net/iessochoa/danielabellan/practica4/MainActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica4/MainActivity.java
@@ -1,8 +1,13 @@
 package net.iessochoa.danielabellan.practica4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcion_ordena:
+                muestraToast("La lista ha sido ordenada");
+                break;
+            case R.id.opcion_acercaDe:
+                muestraAcerca();
+                break;
+        }
+        return true;
+    }
+
+    private void muestraToast(String mensaje){
+        Toast.makeText(getApplicationContext(), mensaje,Toast.LENGTH_SHORT).show();
+    }
+
+    private void muestraAcerca(){
+        AlertDialog.Builder ventanaEmergente = new AlertDialog.Builder(this);
+        ventanaEmergente.setTitle("Acerca de...");
+        ventanaEmergente.setMessage("Práctica 4\nVersión 1.0\nDaniel Abellán Zárate\nLicencia CC\n(Creative Commons)");
+        ventanaEmergente.setPositiveButton(android.R.string.ok, null);
+        ventanaEmergente.show();
+    }
+
 }
