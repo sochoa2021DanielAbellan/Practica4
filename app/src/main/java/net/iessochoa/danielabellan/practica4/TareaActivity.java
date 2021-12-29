f64025c54084c242db91fce6e9d9fa75d1e9d18f
DanielAZ <danielabezarate@gmail.com>
2021-12-29 19:15:06 +0100

    Ahora va

--- app/src/main/java/net/iessochoa/danielabellan/practica4/TareaActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica4/TareaActivity.java
@@ -38,30 +38,28 @@ public class TareaActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarea);
 
-        tvSpinnerCategoria.findViewById(R.id.tvSpinnerCategoria);
-        spTareaCategoria.findViewById(R.id.spTareaCategoria);
-        final ArrayAdapter<CharSequence> adaptadorCategoria = ArrayAdapter.createFromResource(this, R.array.categoriaTarea, android.R.layout.simple_spinner_item);
-        spTareaCategoria.setAdapter(adaptadorCategoria);
+        tvSpinnerCategoria = findViewById(R.id.tvSpinnerCategoria);
+        spTareaCategoria = findViewById(R.id.spTareaCategoria);
+        //final ArrayAdapter<CharSequence> adaptadorCategoria = ArrayAdapter.createFromResource(this, R.array.categoriaTarea, android.R.layout.simple_spinner_item);
+        //spTareaCategoria.setAdapter(adaptadorCategoria);
 
-        tvSpinnerPrioridad.findViewById(R.id.tvSpinnerPrioridad);
-        spTareaPrioridad.findViewById(R.id.spTareaPrioridad);
+        tvSpinnerPrioridad = findViewById(R.id.tvSpinnerPrioridad);
+        spTareaPrioridad = findViewById(R.id.spTareaPrioridad);
 
-        ivSpinnerEstado.findViewById(R.id.ivSpinnerEstado);
-        tvSpinnerEstado.findViewById(R.id.tvSpinnerEstado);
-        spTareaEstado.findViewById(R.id.spTareaEstado);
+        ivSpinnerEstado = findViewById(R.id.ivSpinnerEstado);
+        tvSpinnerEstado = findViewById(R.id.tvSpinnerEstado);
+        spTareaEstado = findViewById(R.id.spTareaEstado);
 
-        tvActivityTareaTecnico.findViewById(R.id.tvActivityTareaTecnico);
-        tilTareaActivityTecnico.findViewById(R.id.tilTareaActivityTecnico);
-        tietTareaActivityTecnico.findViewById(R.id.tietTareaActivityTecnico);
+        tilTareaActivityTecnico = findViewById(R.id.tilTareaActivityTecnico);
+        tietTareaActivityTecnico = findViewById(R.id.tietTareaActivityTecnico);
 
-        tvActivityTareaDescripcion.findViewById(R.id.tvActivityTareaDescripcion);
-        tilTareaActivityTecnico.findViewById(R.id.tilTareaActivityDescripcion);
-        tietTareaActivityDescripcion.findViewById(R.id.tietActivityTareaDescripcion);
+        tilTareaActivityTecnico = findViewById(R.id.tilTareaActivityDescripcion);
+        tietTareaActivityDescripcion = findViewById(R.id.tietActivityTareaDescripcion);
 
-        tvTareaActivityResumen.findViewById(R.id.tvTareaActivityResumen);
-        etTareaActivityResumen.findViewById(R.id.etTareaActivityResumen);
+        tvTareaActivityResumen = findViewById(R.id.tvTareaActivityResumen);
+        etTareaActivityResumen = findViewById(R.id.etTareaActivityResumen);
 
-        fabTareaActivityGuardar.findViewById(R.id.fabTareaActivityGuardar);
+        fabTareaActivityGuardar = findViewById(R.id.fabTareaActivityGuardar);
     }
 
     @Override
